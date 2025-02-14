2d568d14a87033b8f6e29aea25e7e59c2a424b32
MoKi <Mokiwins@gmail.com>
2025-02-13 23:01:04 -0500

    CRUD

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,13 @@
 const db = require("../config/db");
 
+// Create a new user
+async function createUser(name, email) {
+  const query = "INSERT INTO users (name, email) VALUES (?, ?)";
+  const [result] = await db.execute(query, [name, email]);
+  return result.insertId;
+}
+
+// Get all users
 async function getAllUsers() {
   try {
     const [rows] = await db.query("SELECT * FROM users");
@@ -9,5 +17,37 @@ async function getAllUsers() {
     throw error;
   }
 }
+// async function getAllUsers() {
+//   const query = "SELECT * FROM users";
+//   const [rows] = await db.execute(query);
+//   return rows;
+// }
 
-module.exports = { getAllUsers };
+// Get a single user by ID
+async function getUserById(id) {
+  const query = "SELECT * FROM users WHERE id = ?";
+  const [rows] = await db.execute(query, [id]);
+  return rows[0];
+}
+
+// Update a user
+async function updateUser(id, name, email) {
+  const query = "UPDATE users SET name = ?, email = ? WHERE id = ?";
+  const [result] = await db.execute(query, [name, email, id]);
+  return result.affectedRows > 0;
+}
+
+// Delete a user
+async function deleteUser(id) {
+  const query = "DELETE FROM users WHERE id = ?";
+  const [result] = await db.execute(query, [id]);
+  return result.affectedRows > 0;
+}
+
+module.exports = {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
